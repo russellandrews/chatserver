14c9835413260b571b524cc75a7958f9b2686563
Russell Andrews <rwandrews62@gmail.com>
2013-02-09 16:52:02 -0500

    Initial Commit

---
+++ helloworld_server.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+
+var express = require('express');
+
+var app = express();
+
+var server = app.listen(8080);
+
+app.get('/', function(req, response){
+  fs.readFile(__dirname+'/helloworld.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.write(data);
+    response.end();
+  });
+});
+
+app.use('/static', express.static(__dirname + '/static'));
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+
+
+nowjs.on("connect", function(){
+  console.log("Joined: " + this.now.name);
+});
+
+nowjs.on("disconnect", function(){
+  console.log("Left: " + this.now.name);
+});
+
+
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, this.now.group, message);
+};
